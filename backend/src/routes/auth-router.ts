292ae389172b290bac1ea748601b9437061bca66
AdrianoEscarabote <drian.escarabote@hotmail.com>
2026-01-12 12:31:05 -0300

    feat(auth-router): add authenticate user route to authentication router

--- backend/src/routes/auth-router.ts
+++ backend/src/routes/auth-router.ts
@@ -1,5 +1,6 @@
 import express from "express"
 
+import authenticateUserRoute from "./auth/authenticate-user"
 import logoutRoute from "./auth/logout"
 import signInRoute from "./auth/sign-in"
 import signUpRoute from "./auth/sign-up"
@@ -9,5 +10,6 @@ const authRouter = express.Router()
 authRouter.use("/sign-in", signInRoute)
 authRouter.use("/sign-up", signUpRoute)
 authRouter.use("/logout", logoutRoute)
+authRouter.use("/authenticate-user", authenticateUserRoute)
 
 export default authRouter
