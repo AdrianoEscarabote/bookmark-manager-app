5de44dc07e907fb8d6e672fa34a192a664ac2b6d
AdrianoEscarabote <drian.escarabote@hotmail.com>
2026-01-20 15:54:09 -0300

    chore: update package.json to include webpack flags for dev and build scripts, add next-pwa and its types

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from 'next'
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})
+
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'icons.duckduckgo.com',
-        pathname: '/ip3/**',
-      },
-    ],
-  },
+  reactStrictMode: true,
 }
 
-export default nextConfig
+module.exports = withPWA(nextConfig)
