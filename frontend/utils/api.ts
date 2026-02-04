19113a593fc56a4c14818b2f7765d5ab026683b0
AdrianoEscarabote <drian.escarabote@hotmail.com>
2026-02-04 11:36:43 -0300

    feat(api): update baseURL to use a relative path for API requests

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useBookmarksStore } from '@/app/_store/bookmarks'
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:4000',
+  baseURL: '/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 })
